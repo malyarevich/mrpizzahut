aa7cbc9800bb0c6d433f9e4f95b279ee52ac6e9d
malyarock <vanyagoua@gmail.com>
2017-07-18 22:03:27 +0300

    init

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
-$("btn-feel-pizza").mousemove(function(){
+$("id="btn-feel-p"").mousemove(function(){
     alert('Hi');
-    $("#btn-feel-p").css("-webkit-filter", function () {
-       return "grayscale(0)";
-    });
-});
+    $("#this").css("color", "back")};
 
 
 /*$("container-fluid").css("filter", function () {
     "grayscale(0)"
 };
 $("container-fluid").css("-webkit-filter", "grayscale(1)");
+
+
+ $("id="btn-feel-p"").mousemove(function(){
+ alert('Hi');
+ $("#this").css("-webkit-filter", function () {
+ return "grayscale(0)";
+ });
+ });
+
 */
