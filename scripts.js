5ed2811ef0bb604dcbf4970e6f6654251b7df64e
malyarock <vanyagoua@gmail.com>
2017-07-18 22:44:39 +0300

    init

--- scripts.js
+++ scripts.js
@@ -8,9 +8,7 @@ $("container-fluid").css("-webkit-filter", "grayscale(1)");
 
  $("id="btn-feel-p"").mousemove(function(){
  alert('Hi');
- $("#this").css("-webkit-filter", function () {
- return "grayscale(0)";
- });
+
  });
 
 
