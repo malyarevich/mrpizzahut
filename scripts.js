e3fee27081954b78146d75605d13e15274953556
malyarock <vanyagoua@gmail.com>
2017-07-18 23:53:14 +0300

    init

--- scripts.js
+++ scripts.js
@@ -2,22 +2,6 @@
 
 /*
 
- <script type="text/javascript">
- $( document ).ready(function()
- {
- var varfeelp = $( "#btn-feel-p" );
- var varbground = $("#bground");
 
- varfeelp.mouseover(function()
- {
- varbground.css("-webkit-filter", "grayscale(0)");
- });
-
- varfeelp.mouseout(function()
- {
- varbground.css("-webkit-filter", "grayscale(1)");
- });
- });
- </script>
 
 */
