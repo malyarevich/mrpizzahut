c61405fc8bb6f3414f629f1ec0858b007df50888
malyarock <vanyagoua@gmail.com>
2017-07-18 21:34:35 +0300

    init

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 $("btn-feel-pizza").mousemove(function(){
 
     $("container-fluid").css("-webkit-filter", function () {
-        "grayscale(0)"
-    };
-})
+        "grayscale(0)";
+    }
+});
 
 
 /*$("container-fluid").css("filter", function () {
