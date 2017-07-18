7342fa0655517476acc8de1c1116ed652a4689ce
malyarock <vanyagoua@gmail.com>
2017-07-18 21:33:51 +0300

    init

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("btn-feel-pizza").mousemove(function(){
+
+    $("container-fluid").css("-webkit-filter", function () {
+        "grayscale(0)"
+    };
+})
+
+
+/*$("container-fluid").css("filter", function () {
+    "grayscale(0)"
+};
+$("container-fluid").css("-webkit-filter", "grayscale(1)");
+*/
